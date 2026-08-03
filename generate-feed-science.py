7c122a45e277fb0d193dfdb173a155b9f4f79d5e
tasselrex <pxinghua@gmail.com>
2026-08-03 14:15:07 +0800

    Refine search queries for scientific categories

--- generate-feed-science.py
+++ generate-feed-science.py
@@ -18,10 +18,10 @@ QUERIES = [
 #    ("Microscopy / imaging", 'all:microscopy OR all:"image analysis" OR all:"cryo-EM" OR all:"electron microscopy" OR all:imaging'),
 #    ("Analytical chemistry", 'all:"mass spectrometry" OR all:chromatography OR all:spectroscopy OR all:FTIR OR all:Raman'),
 #    ("Regulatory / pharma", 'all:FDA OR all:USP OR all:EMA OR all:"good manufacturing practice" OR all:pharma'),
-    ("Superconductivity", 'all:superconductor OR all:superconductivity'),
-    ("Quantum", 'all:quantum'),
-    ("Tribology", 'all:tribology OR all:tribological'),
-    ("Materials science", 'all:"materials science" OR all:polymer OR all:nanomaterial OR all:degradation OR all:surface'),
+    ("Superconductivity", 'all:superconduct* OR cat:cond-mat.supr-con')
+    ("Quantum", 'all:quantum OR cat:quant-ph')
+    ("Tribology", 'all:tribology OR all:friction OR all:wear OR all:lubrication')
+    ("Materials science", 'cat:cond-mat.mtrl-sci OR all:polymer OR all:nanomaterial OR all:degradation')
 ]
 
 HEADERS = {
